cdc11e3d29cd9b1f320266dd49748f457d4d6776
Nikita <yan957@gmail.com>
2021-05-29 19:19:08 +0300

    Начал делать экран назначений

--- src/routes/mainScreen/treatment/Treatment.js
+++ src/routes/mainScreen/treatment/Treatment.js
@@ -1,6 +1,13 @@
 import * as React from 'react'
-import {StyleSheet, View, Text} from "react-native";
+import {StyleSheet, View, FlatList, Text, TouchableOpacity, Image} from "react-native";
+import {RFValue} from 'react-native-responsive-fontsize';
 
+
+const BUTTONS = [
+    {title: 'Мои назначения', img: '../../assets/systemAssets/my_treatments_icon.png', route: ''},
+    {title: 'Добавить курс лечения', img: '../../assets/systemAssets/my_treatments_icon2.png', route: ''},
+    {title: 'Календарь приема', img: '../../assets/systemAssets/my_treatments_icon2.png', route: ''},
+]
 export default class Treatment extends React.Component {
     constructor(props) {
         super(props);
@@ -10,7 +17,21 @@ export default class Treatment extends React.Component {
     render(){
         return(
             <View style={treatmentStyle.container}>
-                <Text>Treatment</Text>
+                <View style={treatmentStyle.header}>
+                    <Text style={treatmentStyle.headerText}>Назначения</Text>
+                </View>
+                {BUTTONS.map((item) => (
+                    <TouchableOpacity
+                        style={treatmentStyle.buttonStyle}
+                        image={item.img}
+                        title={item.title}
+                        onPress={() => {
+                        }}
+                    >
+                        <Image style={treatmentStyle.icon} source={item.img}/>
+                        <Text>{item.title}</Text>
+                    </TouchableOpacity>
+                ))}
             </View>
         )
     }
@@ -19,5 +40,31 @@ export default class Treatment extends React.Component {
 const treatmentStyle = StyleSheet.create({
     container: {
         flex: 1,
-    }
+        alignItems: 'center'
+    },
+    header: {
+        // backgroundColor: 'blue',
+        width: '100%',
+        height: '16%',
+        justifyContent: 'flex-end'
+    },
+    headerText: {
+        paddingLeft: 30,
+        fontSize: RFValue(20, 580),
+    },
+    buttonStyle: {
+        width: '90%',
+        height: '15%',
+        justifyContent: 'center',
+        margin: 20,
+        borderWidth: 1,
+        // backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 30,
+    },
+    icon: {
+        resizeMode: 'contain',
+        width: 20,
+        height: 20,
+    },
 });
